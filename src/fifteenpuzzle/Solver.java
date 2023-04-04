ccc0f98475766575b5603e90f18fbe38604e538c
Mark-Vu <markvu2003@gmail.com>
2023-04-04 13:58:56 -0700

    updated

--- src/fifteenpuzzle/Solver.java
+++ src/fifteenpuzzle/Solver.java
@@ -2,39 +2,22 @@
 package fifteenpuzzle;
 
 import java.io.BufferedReader;
-import java.io.File;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.util.Arrays;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.PriorityQueue;
+
 
 public class Solver {// the solver will input a board and result in movements
 	// piorityQueue;
-	public int hashCode(){
-		return 0;
-	}
-	public int[][] goal;
 	public static int SIZE; // the size of the board
+	public static int[][] goal;
 
-	private void checkBoard(int[][] board) throws BadBoardException {
-		int[] vals = new int[SIZE * SIZE];
-
-		// check that the board contains all number 0...15
-		for (int i = 0; i < SIZE; i++) {
-			for (int j = 0; j < SIZE; j++) {
-				if (board[i][j]<0 || board[i][j]>=SIZE*SIZE)
-					throw new BadBoardException("found tile " + board[i][j]);
-				vals[board[i][j]] += 1;
-			}
-		}
-
-		for (int i = 0; i < vals.length; i++)
-			if (vals[i] != 1)
-				throw new BadBoardException("tile " + i +
-						" appears " + vals[i] + "");
-
-	}
 	public static void main(String[] args) throws BadBoardException, IOException {
+
 		System.out.println("number of arguments: " + args.length);
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
@@ -48,24 +31,23 @@ public class Solver {// the solver will input a board and result in movements
 
 		// TODO
 		BufferedReader br = new BufferedReader(new FileReader(args[0]));
-		SIZE = (int) br.read() -'0';
+		SIZE = br.read() - '0';
 
-		String a = br.readLine();
-		System.out.println(SIZE);
+		br.readLine();
 
 		int[][] board = new int[SIZE][SIZE];
-		int c1, c2, s;
+		int c1, c2;
 
 		for (int i = 0; i < SIZE; i++) {
 			int count = 0;
 			for (int j = 0; j < SIZE; j++) {
 				c1 = br.read();
 				c2 = br.read();
-				s = br.read();
 				count++;
+				br.read();// skip the space
 				if (count == SIZE) {
 					br.readLine();
-				} // skip the space
+				}
 
 				if (c1 == ' ')
 					c1 = '0';
@@ -77,10 +59,186 @@ public class Solver {// the solver will input a board and result in movements
 		//checkBoard(board);
 		br.close();
 		System.out.println(Arrays.deepToString(board));
+		goal = new int[SIZE][SIZE];
+		int index = 1;
+
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				if (i == SIZE - 1 && j == SIZE - 1) {
+					goal[i][j] = 0;
+					break;
+				}
+				goal[i][j] = index;
+				index++;
+			}
+		}
+
+//		System.out.println("Goal board:");
+//		System.out.println(Arrays.deepToString(goal));
+		Vertex b = solve(board);
+		Stack<String> resultList = new Stack<>();
+		while (b.getParent() != null) {
+			resultList.add(b.getMove());
+			b = b.getParent();
+		}
+
+		System.out.println("solution in main : ");
+		while (!resultList.isEmpty()) {
+			System.out.println(resultList.pop());
+		}
 
 		//File input = new File(args[0]);
 		// solve..
 		//File output = new File(args[1]);
 
 	}
+
+	public static boolean queueContains(PriorityQueue<Vertex> queue, Vertex neighbor) {
+		for (Vertex i : queue) {
+			if (neighbor.equals(i)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static Vertex solve(int[][] start) {
+		Vertex startState = new Vertex(start);
+		startState.setHeuristic(startState.getHeuristic(goal));
+		startState.setF(startState.getF());
+		Vertex result;
+//		HashMap<Integer, Integer> closed = new HashMap<>();
+		HashSet<Integer> closed = new HashSet<>();
+		PriorityQueue<Vertex> q = new PriorityQueue<>();
+		q.add(startState);
+		Vertex goalVertex = new Vertex(goal);
+		System.out.println("duma con cac "+startState.getF());
+		int[][] duma =  { {1,2,3},{4,5,6},{7,7,0} };
+		Vertex dumaVertex = new Vertex(duma);
+		System.out.println(dumaVertex.equals(goalVertex));
+		int calWhile = 0;
+		while (!q.isEmpty()) {
+			calWhile++;
+			//TESTING: printing queue
+			System.out.println("Queue:");
+			for (Vertex i : q) {
+				i.setHeuristic(i.getHeuristic(goal));
+				i.setF(i.getF());
+				System.out.println(i.getMove());
+				System.out.println("f value : " + i.getF());
+				System.out.println("cost: " + i.getDistanceFromStart());
+				System.out.println("hash: "+ i.getHashCode());
+			}
+			System.out.println("End queue--------");
+			Vertex node = q.remove();
+
+			//TESTING : printing the node that popped out
+			System.out.println("Popped out :  " + node.getMove());
+			System.out.println("cost: " + node.getDistanceFromStart());
+			System.out.println("f value : " + node.getF());
+
+
+
+			if (node.getHashCode() == goalVertex.getHashCode()) {
+				result = node;
+//				System.out.println("we have result");
+				return result;
+			}
+
+			closed.add(node.getHashCode());
+
+			System.out.println("-------------------------------");
+			for (Vertex neighbor : node.generateChild()) {
+				if (closed.contains(neighbor.getHashCode())) {
+					continue;
+				}
+				neighbor.setHeuristic(neighbor.getHeuristic(goal));
+				neighbor.setF(neighbor.getF());
+
+				//TESTING : printing out the neighbour
+//				System.out.print(neighbor.getMove() + ": ");
+//				System.out.println(Arrays.deepToString(neighbor.getBoard()));
+//				System.out.println("f value : " + neighbor.getF());
+//				System.out.println("cost: " + neighbor.getDistanceFromStart());
+
+
+				if (!closed.contains(neighbor.getHashCode())) {
+					if (queueContains(q, neighbor)) {
+						Vertex openNeighbor = q.stream().filter(n -> n.equals(neighbor)).findFirst().get();
+						System.out.println("duma cai lon ma");
+						if (openNeighbor.getDistanceFromStart() > neighbor.getDistanceFromStart()) {
+
+							/////////////////////////
+							System.out.println("We have NEIGHJDAD");
+							q.remove(openNeighbor);
+							neighbor.setParent(node);
+							q.add(neighbor);
+							System.out.println("open neighbor: " + openNeighbor.getMove());
+							System.out.println("open f" + openNeighbor.getF());
+							System.out.println("neighbor: " + neighbor.getMove());
+							System.out.println("neighbor f" + neighbor.getF());
+
+						}
+					} else {
+						neighbor.setParent(node);
+						q.add(neighbor);
+					}
+				}
+			}
+			System.out.println("-------------------------------");
+		}
+		return null;
+	}
 }
+//		while (!q.isEmpty()) {
+//			calWhile ++ ;
+//			Vertex curr = q.remove();
+//			System.out.println("Next move: " + curr.getMove());
+//			ArrayList<Vertex> neighbors = curr.generateChild();
+//			System.out.println("---------------------------------------");
+//			for (Vertex u : neighbors) {
+//				System.out.print(u.getMove() + ": ");
+//				System.out.println(Arrays.deepToString(u.getBoard()));
+//				u.setHeuristic(u.getHeuristic(goal));
+//				u.setF(u.getF());
+//				System.out.println("f value : " + u.getF());
+//			}
+//			System.out.println("---------------------------------------");
+//			for (Vertex u:neighbors) {
+//				u.setHeuristic(u.getHeuristic(goal));
+//				u.setF(u.getF());
+//				if (u.getHashCode() == goalVertex.getHashCode()) {
+//					u.setParent(curr);
+//					result = u;
+//					System.out.println("time of while loop executed : " + calWhile);
+//					return result; //Maybe change later
+//				} else {
+//					if (q.contains(u)) {
+//						System.out.println("q contain");
+//						for (Vertex i:q) {
+//							if (i.getHashCode() == u.getHashCode() && i.getF() < u.getF()) {
+//								i.setF(u.getF());
+//								u.setParent(curr);
+//							}
+//						}
+//					} else if (closed.containsKey(u.getHashCode())) {
+//						System.out.println("closed contain: ");
+//						System.out.println(u.getMove());
+//						System.out.println(Arrays.deepToString(u.getBoard()));
+//
+//						if (closed.get(u.getHashCode()) > u.getF()) {
+//							closed.put(u.getHashCode(), u.getF());
+//							q.add(u);
+//							u.setParent(curr);
+//						}
+//					} else {
+//						q.add(u);
+//						u.setParent(curr);
+//					}
+//				}
+//			}
+//			closed.put(curr.getHashCode(), curr.getF());
+//		}
+//		return null;
+//	}
+//}
--- src/fifteenpuzzle/Vertex.java
+++ src/fifteenpuzzle/Vertex.java
@@ -2,35 +2,36 @@ package fifteenpuzzle;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class Vertex {
-    private int hashCode;
+public class Vertex implements Comparable<Vertex>{
     private int[][] board;
     private int heuristic;
     private int distanceFromStart;
     private int f;// fi will be thie price of the
-    private int[][] goal;
     private int[] blankPos;
+    private String move;
+    private Vertex parent;
+    private int hashCode = 0;
+    private int boardLength;
 
     public Vertex() {
 
     }
 
-    public Vertex(int[][] b) {
-        this.goal = new int[board.length][board.length];
-        this.board = b;
-        this.heuristic = getHeuristic();
-        this.f = getF();
+    public Vertex(int[][] board)  {
+        this.board = board;
+        this.boardLength = this.board.length;
         this.distanceFromStart = 0;
-        int index = 1;
-        for ( int i = 0 ; i < board.length; i++){
-            for ( int j = 0 ; j <board.length; j++){
-                if ( i== board.length-1 && j== board.length-2){
-                    this.goal[i][j] = 0;
-                    break;
-                }
-                this.goal[i][j] = index;
-                index++;
+        this.heuristic = 0;
+        this.move = "";
+        this.blankPos = new int[2];
+        this.findBlankPos();
+        final int prime = 31;
+        this.hashCode = 17;
+        for (int[] row : this.board) {
+            for (int val : row) {
+                this.hashCode = 31 * this.hashCode + val;
             }
         }
     }
@@ -39,30 +40,70 @@ public class Vertex {
         int x = this.blankPos[0];
         int y = this.blankPos[1];
         int[][] moves = { { x, y - 1 }, { x, y + 1 }, { x - 1, y }, { x + 1, y } };
+        //strMoves are in reverse order (D -> U and U ->D) because 0 move up means the upper one moves down
+        String[] strMoves = { "R", "L", "D", "U"};
         ArrayList<Vertex> children = new ArrayList<>();
-        for (int[] i : moves) {
-            int[][] child = this.shuffle(x, y, i[0], i[1]);
+        int moveLength = moves.length;
+        for (int i = 0; i < moveLength; i++) {
+            int[][] child = this.shuffle(x, y, moves[i][0], moves[i][1]);
             if (child != null) {
                 Vertex childVertex = new Vertex(child);
+                String m = this.board[moves[i][0]][moves[i][1]] + " " + strMoves[i];
+                childVertex.setMove(m);
                 childVertex.setDist(this.distanceFromStart + 1);
                 children.add(childVertex);
             }
 
         }
+
         return children;
+    }
+
+    public int getHashCode() {
+        return this.hashCode;
+    }
+
+    public int getDistanceFromStart() {
+        return this.distanceFromStart;
+    }
+    public void setMove(String move) {
+        this.move = move;
+    }
+
+
+    public String getMove() {
+        return this.move;
+    }
+
+    public void setParent(Vertex parent) {
+        this.parent = parent;
+    }
+
+    public Vertex getParent() {
+        return this.parent;
     }
 
     public void setDist(int newDist) {
         this.distanceFromStart = newDist;
     }
 
+
+    public void setF(int f) {
+        this.f = f;
+    }
+
+    public void setHeuristic(int h) {
+        this.heuristic = h;
+    }
+
     public int[][] shuffle(int x1, int y1, int x2, int y2) {
         //Move the blank space and return the valid board state
-        if (x2 >= 0 && x2 < this.board.length && y2 >= 0 && y2 < this.board.length) {
-            int[][] temp_puz = new int[board.length][board.length];
+
+        if (x2 >= 0 && x2 < this.boardLength && y2 >= 0 && y2 < this.boardLength) {
+            int[][] temp_puz = new int[this.boardLength][this.boardLength];
             int temp;
-            for (int i = 0; i < this.board.length; i++) {
-                temp_puz[i] = Arrays.copyOf(this.board[i], this.board.length);
+            for (int i = 0; i < this.boardLength; i++) {
+                temp_puz[i] = Arrays.copyOf(this.board[i], this.boardLength);
             }
             temp = temp_puz[x2][y2];
             temp_puz[x2][y2] = temp_puz[x1][y1];
@@ -74,21 +115,93 @@ public class Vertex {
     }
 
     public int getF() {
-        this.f = this.heuristic + this.distanceFromStart; // if we make constructor put this in.
-        return this.f;
+        return this.heuristic + this.distanceFromStart; // if we make constructor put this in.
     }
 
-    public int getHeuristic(){
-        int temp = 0 ;
-
-        for ( int i = 0; i < board.length; i++){
-            for ( int j = 0 ; j < board.length; j++){
-                if (this.board[i][j] != goal[i][j] && this.board[i][j] != 0) {
-                    temp ++;
+    public int[] getCoordinate(int tile, int[][] position) {
+        if (position == null) {
+            position = this.board;
+        }
+        for (int i = 0; i < this.boardLength; i++) {
+            for (int j = 0; j < this.boardLength; j++) {
+                if (position[i][j] == tile) {
+                    return new int[]{i,j};
                 }
             }
         }
-        return temp;
+        return null;
+    }
+    public  int getHeuristic(int[][] goal) {
+        int distance = 0;
+        int n = board.length;
+        HashMap<Integer, int[]> goalPositions = new HashMap<>();
+
+        // Store the goal positions of each number in a map for O(1) lookup
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                goalPositions.put(goal[i][j], new int[]{i, j});
+            }
+        }
+
+        // Calculate the Manhattan distance for each number on the board
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = board[i][j];
+                if (num == 0) {
+                    continue;
+                }
+                int[] goalPos = goalPositions.get(num);
+                distance += Math.abs(i - goalPos[0]) + Math.abs(j - goalPos[1]);
+            }
+        }
+
+        return distance;
     }
 
+    @Override
+    public boolean equals(Object other) {
+        return ((Vertex) other).getHashCode() == this.getHashCode();
+    }
+    //    public int getHeuristic(int[][] goalBoard){
+//        int distance = 0;
+//        for (int i =0; i < this.boardLength; i++) {
+//            for (int j = 0; j < this.boardLength; j++) {
+//                int[] coordinates = this.getCoordinate(this.board[i][j], goalBoard);
+//                int i1 = coordinates[0];
+//                int j1 = coordinates[1];
+//                distance += Math.abs(i - i1) + Math.abs(j - j1);
+//            }
+//        }
+//        return distance;
+//    }
+
+    public void findBlankPos() {
+        for (int i = 0; i < this.boardLength; i++) {
+            for (int j = 0; j < this.boardLength; j++) {
+                if (this.board[i][j] == 0) {
+                    this.blankPos[0] = i;
+                    this.blankPos[1] = j;
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int compareTo(Vertex other) {
+
+        if (other.f == this.f) {
+//          System.out.println("other f " + other.f + " this f " + this.f);
+//          System.out.println(" this get dist " + this.getDistanceFromStart() + " , other get dist " + other.getDistanceFromStart());
+//          System.out.println(Integer.compare(this.getDistanceFromStart(), other.getDistanceFromStart()));
+            return Integer.compare(this.getDistanceFromStart(), other.getDistanceFromStart());
+        }
+
+//        return other.f < this.f ? 1 : -1;
+        return Integer.compare(this.getF(), other.getF());
+    }
+
+    public int[][] getBoard() {
+        return this.board;
+    }
 }
