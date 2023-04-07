9868237c57daded26f3267494f672ee5028d63fd
paul <kohleangpaul911@gmail.com>
2023-04-07 10:57:43 -0700

    rearranged code

--- src/fifteenpuzzle/Solver.java
+++ src/fifteenpuzzle/Solver.java
@@ -12,13 +12,11 @@ import java.util.PriorityQueue;
 
 
 public class Solver {// the solver will input a board and result in movements
-	// piorityQueue;
-	public static int SIZE; // the size of the board
+	public static int SIZE; //the dimension of the board
 	public static int[][] goal;
 
-
-
 	public static void main(String[] args) throws BadBoardException, IOException {
+
 		long startTime = System.currentTimeMillis();
 
 		System.out.println("number of arguments: " + args.length);
@@ -32,7 +30,7 @@ public class Solver {// the solver will input a board and result in movements
 			return;
 		}
 
-		// TODO
+
 		BufferedReader br = new BufferedReader(new FileReader(args[0]));
 		SIZE = (int) br.read() -'0';
 
@@ -42,6 +40,7 @@ public class Solver {// the solver will input a board and result in movements
 		int[][] board = new int[SIZE][SIZE];
 		int c1, c2, s;
 
+		//convert the board into 2D array
 		for (int i = 0; i < SIZE; i++) {
 			for (int j = 0; j < SIZE; j++) {
 				c1 = br.read();
@@ -60,11 +59,13 @@ public class Solver {// the solver will input a board and result in movements
 		}
 		br.close();
 
-		System.out.println(Arrays.deepToString(board));
+		System.out.println("INITIAL board : " + Arrays.deepToString(board));
+
 
 		goal = new int[SIZE][SIZE];
 		int index = 1;
 
+		//creating the goal board
 		for (int i = 0; i < SIZE; i++) {
 			for (int j = 0; j < SIZE; j++) {
 				if (i == SIZE - 1 && j == SIZE - 1) {
@@ -76,22 +77,23 @@ public class Solver {// the solver will input a board and result in movements
 			}
 		}
 
-
 		Vertex b = solve(board);
+
+
 		Stack<String> resultList = new Stack<>();
 		while (b != null) {
 			resultList.add(b.getMove());
 			b = b.getParent();
 		}
+		resultList.pop();
 
-		System.out.println("solution in main : ");
+		System.out.println("\nSOLUTION : ");
 		while (!resultList.isEmpty()) {
 			System.out.println(resultList.pop());
 		}
 
 
-		// Your code here
-
+		//calculating run time
 		long endTime = System.currentTimeMillis();
 		long runtime = endTime - startTime;
 		if ( runtime > 30000) {
@@ -120,25 +122,13 @@ public class Solver {// the solver will input a board and result in movements
 		PriorityQueue<Vertex> q = new PriorityQueue<>();
 		q.add(startState);
 		Vertex goalVertex = new Vertex(goal);
-		//<hashcode , F value>
 
 		HashMap<Long, Integer> closed = new HashMap<>();
 
 		while (!q.isEmpty()) {
-			System.out.println("queue size : " + q.size());
 			Vertex curr = q.remove();
 			ArrayList<Vertex> neighbors = curr.generateChild();
-//				System.out.println("---------------------------------------");
-			//for (Vertex u : neighbors) {
-//					System.out.print(u.getMove() + ": ");
-//					System.out.println(Arrays.deepToString(u.getBoard()));
-			//	u.setHeuristic(u.getHeuristic(goal));
-			//	u.setF(u.getF());
-//						System.out.println("heuristic : " + u.getHeuristic(goal));
-//						System.out.println("cost : " + u.getDistanceFromStart());
-//						System.out.println("f value : " + u.getF());
-			//}
-//				System.out.println("---------------------------------------");
+
 			for (Vertex neighbor:neighbors) {
 
 				neighbor.setHeuristic(neighbor.getHeuristic(goal));
@@ -153,13 +143,6 @@ public class Solver {// the solver will input a board and result in movements
 					Vertex inQueue = queueContains(q,neighbor);
 					if (inQueue != null) {//
 						if (inQueue.getF() > neighbor.getF()) {
-//								System.out.println("neighbor in queue code: "+ openNeighbor.getHashCode());
-//								System.out.println("i board: "+ Arrays.deepToString(openNeighbor.getBoard()));
-//								System.out.println("neighbor code: "+ neighbor.getHashCode());
-//								System.out.println("neighbor board: "+ Arrays.deepToString(neighbor.getBoard()));
-//									queueElement.setF(neighbor.getF());
-//									neighbor.setParent(curr); // change the value of the vertex in the piority queue, does it change the order?
-							// maybe we have to remove the node and then add it in
 							q.remove(inQueue);
 							neighbor.setParent(curr);
 							q.add(neighbor);
@@ -168,7 +151,6 @@ public class Solver {// the solver will input a board and result in movements
 					}
 					else if (closed.containsKey(neighbor.getHashCode())) {
 						if (closed.get(neighbor.getHashCode()) > neighbor.getF()) {
-							System.out.println("ahihi con cac du ma cay vai ca lone");
 							closed.put(neighbor.getHashCode(), neighbor.getF());
 							q.add(neighbor);
 							neighbor.setParent(curr);
@@ -183,7 +165,6 @@ public class Solver {// the solver will input a board and result in movements
 			closed.put(curr.getHashCode(), curr.getF());
 
 		}
-		System.out.println("duma empty queue");
 		return null;
 	}
 }
